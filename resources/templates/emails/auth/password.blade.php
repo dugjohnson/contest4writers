9af1f838bc27c1eb08f7c3f794a540e0b71833ff
Doug Johnson <doug@asknice.com>
2015-01-07 04:26:42 -0600

    Reset password working, maybe

--- resources/templates/emails/auth/password.blade.php
+++ resources/templates/emails/auth/password.blade.php
@@ -1,5 +1,17 @@
-Following the link below will allow you to reset your password.
-If you did not request this change, you need do nothing.
-If the link below does not appear as a clickable link you can paste the link into your browser.
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<h3>Reset your password</h3>
+<p>Following the link below will allow you to reset your password.</p>
+<p>If you did not request this change, you need do nothing.</p>
+<p>If the link below does not appear as a click-able link you can paste the link into your browser.</p>
 
-
+<div>
+    To reset your password, complete this form: {!! HTML::link(URL::to('password/reset', array($token))) !!}.<br/>
+    This link will expire in {!! Config::get('auth.reminder.expire', 60) !!} minutes.
+</div>
+</body>
+</html>
--- resources/templates/judge/show.blade.php
+++ resources/templates/judge/show.blade.php
@@ -1,8 +1,72 @@
-<?php
-/**
- * Created by IntelliJ IDEA.
- * User: doug
- * Date: 1/4/2015
- * Time: 2:24 PM
- */
-        
+@extends('layout')
+
+@section('content')
+    <h3><strong>Contact information</strong></h3>
+    <p>First Name: {!! firstName !!}
+    Last Name; {!! lastName !!}</p>
+    <p>Writing/Author Name; {!! authorName !!}</p>
+    <p>Email (Primary): {!! email1 !!}</p>
+    <p>Email (Secondary): {!! email2 !!}</p>
+    <p>Author Email: {!! email3 !!}</p>
+    <p>Daytime Phone: {!! phone1 !!}
+    <p> Evening Phone: {!! phone1 !!}
+    Address to mail books (published only)
+    If you want to judge published, please fill in the following:
+
+    Street: {!! xx !!}
+    City: {!! xx !!}
+    State: {!! xx !!}
+    Zip: {!! xx !!}
+    Country: {!! xx !!}
+
+    Judging this year: {!! xx !!}
+
+
+    I will judge:
+
+    Published: {!! xx !!} (drop down yes/no)
+    Maximum number of published: {!! xx !!} (auto insert 5)
+    Unpublished: {!! xx !!} (drop down yes/no)
+    Maximum number of unpublished: {!! xx !!} (auto insert 6)
+    Either (not both): {!! xx !!} I’ve indicated the max. number above (drop down yes/no)
+
+    Category Preferences
+
+    If you have a definite preference for or against a category, please select Top Choice or
+    Will Not Judge from the list
+        Top choice, Will Love to Judge, Would Judge if Asked, Will not Judge
+    Mainstream: {!! mainstream !!}
+    Category: {!! category !!}
+    Historical: {!! historical !!}
+    Single Title: {!! singleTitle !!}
+    Paranormal: {!! paranormal !!}
+    Inspirational: {!! inspirational !!}
+
+    I’d be happy to judge a story with these elements (check all that apply)
+
+    Erotic or high heat: {!! xx !!} (check boxes)
+    GLBT: {!! xx !!}
+    BSDM: {!! xx !!}
+    Vampires and/or Werewolves: {!! xx !!}
+    Religious and/or inspirational content: {!! xx !!}
+
+    Special Instructions/Comments or Notes: (If you’ll be in a different location before
+    published books are shipped, please note that here.): {!! xx !!}
+
+    Comments or Notes (text box): {!! xx !!}
+
+    Thank you for taking the time to visit the judge database for the Daphne du Maurier
+    Award for Excellence
+    Your current selections have been noted and are repeated below for your review.
+
+    Please record your judge number and mark your calendars for these deadlines
+    Deadline for judging Unpublished entries is 15th of April
+    Deadline for judging Published books is 15th of May
+    If anything should change before March 15th, please visit this site
+    again http://contest.rwamysterysuspense.org/judges or contact: Nancy J Nicholson
+    at: ndjnich@gmail.com.
+
+    Thanks,
+
+    Daphne Committee
+@stop
