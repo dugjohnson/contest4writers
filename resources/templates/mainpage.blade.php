65c5b7eea793d78f998ea0bc0747a0a40e79c85e
Doug Johnson <doug@asknice.com>
2015-01-14 14:31:27 -0600

    More formatting changes

--- resources/templates/mainpage.blade.php
+++ resources/templates/mainpage.blade.php
@@ -2,8 +2,22 @@
 
 @section('content')
     <div class="panel">
-        <p>This is where information about the competition will go.</p>
-        <p>Also some kind of a greeting would be a good idea</p>
-        <p>There's room at the right for instructions</p>
+        <p>Welcome to the Daphne du Maurier Award for Excellence in Mystery/Suspense.</p>
+
+        <p> Bookmark this page for all your Daphne needs.
+        </p>
+
+        <p> Questions about the Daphne and categories to enter or judge, please visit <a
+                    href="http://rwamysterysuspense.org/daphne">http://rwamysterysuspense.org/daphne</a>
+        </p>
+
+        <p> This site is now a one stop for all contestant, publishers and judges. If you've been a judge in the last
+            two years please log in using your primary email address.
+        </p>
+
+        <p>All others, please register.</p>
+
+        <p> Once signed in and registered, please edit your profile to see if any information is changed. Visit the edit
+            profile during the contest if you have made any changes.</p>
     </div>
 @stop
