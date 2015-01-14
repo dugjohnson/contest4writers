9829ba35ced26310e5962ebff8bcce3dbc8e5883
Doug Johnson <doug@asknice.com>
2015-01-14 17:19:39 -0600

    Show the downloaded file for retrieval

--- resources/templates/entry/showunpub.blade.php
+++ resources/templates/entry/showunpub.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     @include('entry.formunpub')
+    <a href="/uploads/entries/{{ $entry->filename }}" class="small button">Click to download {{ $entry->filename }}</a><br/>
     <a href="/entries" class="button radius">Back to Entries</a>
 
 @stop
