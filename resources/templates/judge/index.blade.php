3fbfc0372d0577efbc6676527b554c0ac6d26b21
Doug Johnson <doug@asknice.com>
2015-01-18 19:12:02 -0600

    Set debug flag for config to not get the terrible messages. Update the message on the judge page to let them know it is under construction

--- resources/templates/judge/index.blade.php
+++ resources/templates/judge/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layout')
 @section('content')
     <p>You have arrived at the Judge Page</p>
+    <p>We are still under construction and will be sending out an email as soon as we are open for you
+        to update your judge preferences.  Or feel free to check back any time. It's just a click away</p>
+    <p>We're working hard to make judging even easier and more fun for you. It will be worth the wait.</p>
 @stop
 
