bc2946c1f9cadad4bd2455c43628a7cc6103ed6a
Doug Johnson <doug@asknice.com>
2015-01-24 14:38:26 -0600

    Update AuthController to handle change in laravel

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,6 +2,8 @@
 
 use Contest\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\Registrar;
 
 class AuthController extends Controller {
 
@@ -15,6 +17,20 @@ class AuthController extends Controller {
 	| a simple trait to add these behaviors. Why don't you explore it?
 	|
 	*/
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
+	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+	 * @return void
+	 */
+	public function __construct(Guard $auth, Registrar $registrar)
+	{
+		$this->auth = $auth;
+		$this->registrar = $registrar;
+
+		$this->middleware('guest', ['except' => 'getLogout']);
+	}
 
 	use AuthenticatesAndRegistersUsers;
 
