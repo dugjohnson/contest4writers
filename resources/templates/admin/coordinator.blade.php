96427127ad54419c768285b3eb5dea71a9df03ce
Doug Johnson <doug@asknice.com>
2015-02-08 14:37:19 -0600

    Limit judge access to admins

--- resources/templates/admin/coordinator.blade.php
+++ resources/templates/admin/coordinator.blade.php
@@ -1,6 +1,8 @@
 <div class="nav-bar">
     <ul class="button-group">
         <li><a href="/coordinators/entries" class="button radius">Entry Access</a></li>
-        <li><a href="/coordinators/judges" class="button radius">Judge Access</a></li>
+        @if($isAdmin)
+            <li><a href="/coordinators/judges" class="button radius">Judge Access</a></li>
+        @endif
     </ul>
 </div>
