330894d181e397a7e55b38c91634dc95f1c5d00d
Doug Johnson <doug@asknice.com>
2015-02-09 04:56:04 -0600

    Add judge CSV download Update CA as capped

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ use Contest\Entry;
 use Contest\Http\Requests;
 use Contest\Http\Controllers\Helpers\EntryHelper;
 use Contest\Judge;
+use Illuminate\Support\Facades\Response;
 
 class AdminController extends Controller
 {
@@ -46,8 +47,8 @@ class AdminController extends Controller
 
     public function judges()
     {
-        
-      //todo: Make this work with roles  $judges = Judge::whereRaw($this->getRolesWhereClause($this->adminPerson))->orderBy('category')->orderBy('published')->get();
+
+        //todo: Make this work with roles  $judges = Judge::whereRaw($this->getRolesWhereClause($this->adminPerson))->orderBy('category')->orderBy('published')->get();
         $judges = Judge::with('user')
             ->join('users', 'users.id', '=', 'judges.user_id')
             ->orderBy('judgeThisYear')
@@ -58,4 +59,33 @@ class AdminController extends Controller
 
     }
 
+    public function returnCSV()
+    {
+        $judges = Judge::with('user')
+            ->join('users', 'users.id', '=', 'judges.user_id')
+            ->orderBy('users.lastName')
+            ->get();
+
+        // the csv file with the first row
+        $output = implode(",", array('Judge ID', 'Judge name', 'This Year', 'Pub', 'Pub Max', 'Unpub', 'Unpub Max', 'ENB', 'MA', 'CA', 'HI', 'ST', 'PA', 'IN'));
+
+        foreach ($judges as $row) {
+            // iterate over each tweet and add it to the csv
+            $output .= "\r".implode(",", array($row->user_id, $row->judgeName(), $row->judgeThisYear, $row->judgePub,
+                $row->maxpubentries, $row->judgeUnpub, $row->maxunpubentries, $row->judgeEitherNotBoth, $row->mainstream,
+                $row->category, $row->historical, $row->singleTitle, $row->paranormal, $row->inspirational)); // append each row
+        }
+
+        // headers used to make the file "downloadable", we set them manually
+        // since we can't use Laravel's Response::download() function
+        $headers = array(
+            'Content-Type' => 'text/csv',
+            'Content-Disposition' => 'attachment; filename="judges.csv"',
+        );
+
+        // our response, this will be equivalent to your download() but
+        // without using a local file
+        return Response::make(rtrim($output, "\n"), 200, $headers);
+
+    }
 }
--- resources/templates/admin/administrator.blade.php
+++ resources/templates/admin/administrator.blade.php
@@ -1,5 +1,6 @@
 <div class="nav-bar">
     <ul class="button-group">
         <li><a href="/reports" class="button radius">Reports</a></li>
+        <li><a href="/administrators/download" class="button radius">Download Judge CSV</a></li>
     </ul>
 </div>
