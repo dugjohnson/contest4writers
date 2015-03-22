73fc958bdc7b625f4d31b11d300ffa0055268efd
Doug Johnson <doug@asknice.com>
2015-03-22 13:02:38 -0500

    Added color to access

--- resources/templates/admin/scoresheets/access.blade.php
+++ resources/templates/admin/scoresheets/access.blade.php
@@ -23,9 +23,13 @@
                     <td>{!! $categories[ $scoresheet->category ] !!}</td>
                     <td>{!! ($scoresheet->published?'Published':'Unpublished') !!}</td>
                     @if($scoresheet->finalScore == 0)
-                        <td>Not Started</td>
+                        <td class="not-started">Not Started</td>
                     @else
-                        <td>{!! ($scoresheet->completed?'Completed':'Started') !!}</td>
+                        @if ($scoresheet->completed)
+                            <td class="completed">Completed</td>
+                        @else
+                            <td class="started">Started</td>
+                        @endif
                     @endif
                     <td>{{$scoresheet->finalScore}}</td>
                     @if(isset($scoresheet->judge))
