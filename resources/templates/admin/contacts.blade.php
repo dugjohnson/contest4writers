9e28edc2fa255954320955037de95efbdab703ae
Doug Johnson <doug@asknice.com>
2015-03-26 18:31:47 -0500

    Fix score sheet

--- resources/templates/admin/contacts.blade.php
+++ resources/templates/admin/contacts.blade.php
@@ -4,9 +4,9 @@
     <p><strong>Mainstream:</strong> <a href="mailto:veronicaforand@gmail.com?subject=Entry Question">Veronica Forand (veronicaforand@gmail.com)</a></p>
     <p><strong>Category (Series):</strong>  <a href="mailto:bgerry2@yahoo.com?subject=Entry Question">Barbara Gerry (bgerry2@yahoo.com)</a></p>
     <p><strong>Single Title:</strong>  <a href="mailto:bgerry2@yahoo.com?subject=Entry Question">Barbara Gerry (bgerry2@yahoo.com)</a></p>
-    <p><strong>Historical:</strong>  <a href="mailto:islagreyauthor@gmail.com?subject=Entry Question">Isla Grey (islagreyauthor@gmail.com)</a></p>
-    <p><strong>Inspirational:</strong>  <a href="mailto:islagreyauthor@gmail.com?subject=Entry Question">Isla Grey (islagreyauthor@gmail.com)</a></p>
-    <p><strong>Paranormal</strong>:  <a href="mailto:islagreyauthor@gmail.com?subject=Entry Question">Isla Grey (islagreyauthor@gmail.com)</a></p>
+    <p><strong>Historical:</strong>  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
+    <p><strong>Inspirational:</strong>  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
+    <p><strong>Paranormal</strong>:  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
     <h5><strong>Published Coordinators:</strong></h5>
     <p><strong>Mainstream:</strong>  <a href="mailto:Rkhan@wcc.net?subject=Entry Question">Rashda Khan (Rkhan@wcc.net)</a></p>
     <p><strong>Paranormal:</strong>  <a href="mailto:Rkhan@wcc.net?subject=Entry Question">Rashda Khan (Rkhan@wcc.net)</a></p>
