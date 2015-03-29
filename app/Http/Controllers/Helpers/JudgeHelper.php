c8a2095d72e571143f3aab20845d63ddf194de0f
Doug Johnson <doug@asknice.com>
2015-03-29 11:15:12 -0500

    Add ability for OC or JC to add and see internal comments

--- app/Http/Controllers/Helpers/JudgeHelper.php
+++ app/Http/Controllers/Helpers/JudgeHelper.php
@@ -11,12 +11,12 @@ namespace Contest\Http\Controllers\Helpers;
 
 trait JudgeHelper {
 
-    public $preferenceLevels = ['4'=>'Top choice', '3'=>'Love to judge','2'=>'Would judge if asked','1'=>'Would judge in an emergency','0'=>'Will not judge'];
-    public $judgeThisYear = [''=>'Please indicate your participation this year','LJ'=>'I would love to judge this year', 'NY'=>'Ask me next year','EJ'=>'Use me in an emergency','RM'=>'Please remove me from judging list'];
-    
-    public function judgeFormData($judge){
-       return array('judge' => $judge,'preferenceLevels'=>$this->preferenceLevels,'judgeThisYear'=>$this->judgeThisYear,'isCoordinator'=>$this->isCoordinator);
-        
-    }
+	public $preferenceLevels = [ '4' => 'Top choice', '3' => 'Love to judge', '2' => 'Would judge if asked', '1' => 'Would judge in an emergency', '0' => 'Will not judge' ];
+	public $judgeThisYear = [ '' => 'Please indicate your participation this year', 'LJ' => 'I would love to judge this year', 'NY' => 'Ask me next year', 'EJ' => 'Use me in an emergency', 'RM' => 'Please remove me from judging list' ];
+
+	public function judgeFormData( $judge ) {
+		return array( 'judge' => $judge, 'preferenceLevels' => $this->preferenceLevels, 'judgeThisYear' => $this->judgeThisYear, 'isCoordinator' => $this->isCoordinator, 'isAdmin' => $this->isAdmin );
+
+	}
 
 }
--- resources/templates/judge/edit.blade.php
+++ resources/templates/judge/edit.blade.php
@@ -5,5 +5,8 @@
     @include('errors')
     {!! Form::open(array('url' => 'judges/'.$judge->id,'method'=>'put'))  !!}
     @include('judge.form')
+    @if($isAdmin)
+        @include('judge.formAdmin')
+    @endif
     @include('judge.formclose')
 @stop
