d91b0da515059b76ff90f16f2f3222be97183969
Doug Johnson <doug@asknice.com>
2016-01-15 05:17:19 -0600

    Update contacts with new coordinators

--- resources/templates/admin/contacts.blade.php
+++ resources/templates/admin/contacts.blade.php
@@ -1,12 +1,12 @@
 <div class="content">
     <h5><strong>Contacts</strong></h5>
-<!--    <h5><strong>Unpublished Coordinators</strong></h5>
+    <h5><strong>Unpublished Coordinators</strong></h5>
     <p><strong>Mainstream:</strong> <a href="mailto:veronicaforand@gmail.com?subject=Entry Question">Veronica Forand (veronicaforand@gmail.com)</a></p>
-    <p><strong>Category (Series):</strong>  <a href="mailto:bgerry2@yahoo.com?subject=Entry Question">Barbara Gerry (bgerry2@yahoo.com)</a></p>
-    <p><strong>Single Title:</strong>  <a href="mailto:bgerry2@yahoo.com?subject=Entry Question">Barbara Gerry (bgerry2@yahoo.com)</a></p>
-    <p><strong>Historical:</strong>  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
-    <p><strong>Inspirational:</strong>  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
-    <p><strong>Paranormal</strong>:  <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
+    <p><strong>Category (Series):</strong>  <a href="mailto:daphnestc@gmail.com?subject=Entry Question">Cathy Perkins (daphnestc@gmail.com)</a></p>
+    <p><strong>Single Title:</strong>  <a href="mailto:daphnestc@gmail.com?subject=Entry Question">Cathy Perkins (daphnestc@gmail.com)</a></p>
+    <p><strong>Historical:</strong>  <a href="mailto:marneejo@hotmail.com?subject=Entry Question">Marnee Blake (marneejo@hotmail.com)</a></p>
+    <p><strong>Inspirational:</strong>  <a href="mailto:marneejo@hotmail.com?subject=Entry Question">Marnee Blake (marneejo@hotmail.com)</a></p>
+    <p><strong>Paranormal</strong>:  <a href="mailto:marneejo@hotmail.com?subject=Entry Question">Marnee Blake (marneejo@hotmail.com)</a></p>
     <h5><strong>Published Coordinators:</strong></h5>
     <p><strong>Mainstream:</strong>  <a href="mailto:Rkhan@wcc.net?subject=Entry Question">Rashda Khan (Rkhan@wcc.net)</a></p>
     <p><strong>Paranormal:</strong>  <a href="mailto:Rkhan@wcc.net?subject=Entry Question">Rashda Khan (Rkhan@wcc.net)</a></p>
@@ -15,9 +15,10 @@
     <p><strong>Historical:</strong>  <a href="mailto:itraci@hotmail.com?subject=Entry Question">Traci Andrighetti (itraci@hotmail.com)</a></p>
     <p><strong>Inspirational:</strong>  <a href="mailto:itraci@hotmail.com?subject=Entry Question">Traci Andrighetti (itraci@hotmail.com)</a></p>
     <h5><strong>Administrative Coordinators</strong></h5>
--->
+
     <p><strong>Overall Coordinator:</strong> <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
-<!--    <p><strong>First Round Judge Coordinator:</strong> <a href="mailto:ndjnich@gmail.com?subject=Entry Question">Nancy Nicholson (ndjnich@gmail.com)</a></p>
+    <p><strong>First Round Judge Recruiting Coordinator:</strong> <a href="mailto:janiecrouch@verizon.net?subject=Entry Question">Janie Crouch (janiecrouch@verizon.net)</a></p>
+    <p><strong>First Round Judge Liaison Coordinator:</strong> <a href="mailto:jengraybeal@gmail.com?subject=Entry Question">Jen Graybeal (jengraybeal@gmail.com)</a></p>
     <p><strong>Final Round Judge Coordinator:</strong> <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
-    -->
+
 </div>
