bc493b4116311a98aaea78db118d64ce7aaa4235
Doug Johnson <doug@asknice.com>
2016-05-30 09:30:28 -0500

    Update text for pub non-finals

--- resources/templates/admin/closeout/emails/pubnonfinal.blade.php
+++ resources/templates/admin/closeout/emails/pubnonfinal.blade.php
@@ -1,11 +1,17 @@
-<p>Dear 2016 Daphne Entrant:</p>
+<p>Dear Daphne Published Entrant:</p>
 
-<p>The 2016 Daphne Published Contest has ended and unfortunately your entry did not final. This year's published contest was highly competitive and what one judge loves, another doesn't. Take this opportunity to look over the comments with an eye to seeing an Aha moment you hadn't considered in your writing and making the next book even better.
+<p>The 2016 Daphne Published Contest has ended and unfortunately your entry did not final.
+	This year's published contest, as always, was highly competitive. What one judge loves, another doesn't.
+	Take this opportunity to look over the comments (if any) with an eye to maybe seeing an "Aha moment" you hadn't considered in
+	your writing. Perhaps they will even help in making the next book even better.
 </p>
 <p>If you wish to send a thank you note to your judge, you may send them to me and I will forward.
 </p>
-<p>Thank you for entering the Daphne du Maurier Award for Excellence in Mystery/Suspense. On behalf of the Daphne Committee, I wish you the very best in your writing career.
+<p>Thank you for entering the Daphne du Maurier Award for Excellence in Mystery/Suspense.
+	On behalf of the Daphne Committee, I wish you the very best in your writing career.
 </p>
 
-<p>Sincerely,</p>
-<p>{!!$coordinator!!}</p>
+<p>Best,<br>
+Brooke</p>
+<p>Brooke Wills<br>
+2016 Daphne Contest Chair</p>
