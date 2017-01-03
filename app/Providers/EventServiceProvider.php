72d0390b234a04788b3c3f2bbcebc69a841e0212
Doug Johnson <doug@asknice.com>
2017-01-02 18:36:36 -0600

    Updated to Laravel 5.3 new install of Laravel before importing

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -1,18 +1,32 @@
-<?php namespace Contest\Providers;
+<?php
 
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
-class EventServiceProvider extends ServiceProvider {
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'App\Events\SomeEvent' => [
+            'App\Listeners\EventListener',
+        ],
+    ];
 
-	/**
-	 * The event handler mappings for the application.
-	 *
-	 * @var array
-	 */
-	protected $listen = [
-		'event.name' => [
-			'EventListener',
-		],
-	];
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
 
+        //
+    }
 }
--- app/User.php
+++ app/User.php
@@ -1,139 +1,29 @@
-<?php namespace Contest;
+<?php
 
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+namespace App;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
 {
-
-    use Authenticatable, CanResetPassword;
-
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
-    protected $table = 'users';
-
-    protected $rolesList = null;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['email', 'password'];
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
     /**
-     * The attributes excluded from the model's JSON form.
+     * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
-
-    protected function hasRoles()
-    {
-        if (is_null($this->rolesList)) {
-            $this->rolesList = Role::where('user_id', '=', $this->id)->get();
-        }
-        return count($this->rolesList) > 0;
-    }
-
-    public function entries()
-    {
-        return $this->hasMany('Contest\Entry');
-    }
-
-    public function isEntrant()
-    {
-        if ($this->entries()->count()>0) {
-            //todo: Add in check for as author, not entrant
-            return true;
-        }
-        return false;
-
-    }
-
-    public function judge()
-    {
-        return $this->hasOne('Contest\Judge');
-    }
-
-    public function isJudge()
-    {
-        if ($this->judge) {
-            return true;
-
-        }
-        return false;
-
-    }
-
-
-    public function roles()
-    {
-        return $this->hasMany('Contest\Role', 'user_id');
-    }
-
-    public function getRoles()
-    {
-        if (!$this->hasRoles()) {
-            return null;
-        };
-        return $this->rolesList;
-    }
-
-    public function hasFilledInProfile()
-    {
-        return ('' <> $this->lastName);
-
-    }
-
-    public function hasAccessTo($category, $published)
-    {
-        $hasAccess = false;
-        if (!$this->hasRoles()) {
-            return $hasAccess;
-        };
-        foreach ($this->rolesList as $role) {
-            if (('OC' == $role->role) || ('JC' == $role->role) || ('CC' == $role->role && $category == $role->category && $published == $role->published)) {
-                $hasAccess = true;
-                break;
-            }
-        }
-        return $hasAccess;
-
-    }
-
-    public function isCoordinator()
-    {
-        return ($this->isAdministrator() or $this->hasRole());
-
-    }
-
-    public function isAdministrator()
-    {
-        return ($this->hasRole('OC') || $this->hasRole('JC'));
-
-    }
-
-    public function hasRole($type = 'CC')
-    {
-        $hasAccess = false;
-        if (!$this->hasRoles()) {
-            return $hasAccess;
-        };
-        foreach ($this->rolesList as $role) {
-            if ($type == $role->role) {
-                $hasAccess = true;
-                break;
-            }
-        }
-        return $hasAccess;
-
-    }
-
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
