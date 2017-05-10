aecfa5cac526ef9c3d22f497ee6b8a86d5338db0
Doug Johnson <doug@asknice.com>
2017-05-10 16:08:54 -0500

    Fixing problem with sending out finalist/non-finalist emails

--- app/Http/Controllers/CloseoutController.php
+++ app/Http/Controllers/CloseoutController.php
@@ -9,6 +9,7 @@ use Contest\Judge;
 use Contest\Scoresheet;
 use Contest\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Mail;
