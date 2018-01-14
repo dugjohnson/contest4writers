a9919c11c71a88ee738cf3ecf1c124d140a4c55c
Doug Johnson <doug@asknice.com>
2018-01-13 18:44:42 -0600

    update admin contacts

--- dugjohnson-administration/views/admin/contacts.blade.php
+++ dugjohnson-administration/views/admin/contacts.blade.php
@@ -2,18 +2,18 @@
     <h5><strong>Contacts</strong></h5>
     <h5><strong>Unpublished Coordinators</strong></h5>
     <p><strong>Mainstream:</strong> <a href="mailto:veronicaforand@gmail.com?subject=Entry Question">Veronica Forand (veronicaforand@gmail.com)</a></p>
-    <p><strong>Category (Series):</strong>  <a href="mailto:(marnee.blake@gmail.com?subject=Entry Question">Marnee Blake (marnee.blake@gmail.com)</a></p>
-    <p><strong>Single Title:</strong>  <a href="mailto:(marnee.blake@gmail.com?subject=Entry Question">Marnee Blake (marnee.blake@gmail.com)</a></p>
+    <p><strong>Category (Series):</strong>  <a href="mailto:(lesliekod@gmail.com?subject=Entry Question">Leslie Scott  (lesliekod@gmail.com)</a></p>
+    <p><strong>Single Title:</strong>  <a href="mailto:(lesliekod@gmail.com?subject=Entry Question">Leslie Scott (lesliekod@gmail.com)</a></p>
     <p><strong>Historical:</strong>  <a href="mailto:tracybrodybooks@yahoo.com?subject=Entry Question">Tracy Brody (tracybrodybooks@yahoo.com)</a></p>
     <p><strong>Inspirational:</strong>  <a href="mailto:tracybrodybooks@yahoo.com?subject=Entry Question">Tracy Brody (tracybrodybooks@yahoo.com)</a></p>
     <p><strong>Paranormal</strong>:  <a href="mailto:tracybrodybooks@yahoo.com?subject=Entry Question">Tracy Brody (tracybrodybooks@yahoo.com)</a></p>
     <h5><strong>Published Coordinators:</strong></h5>
-    <p><strong>Mainstream:</strong>  <a href="mailto:avery@averyflynn.com?subject=Entry Question">Avery Flynn (avery@averyflynn.com)</a></p>
-    <p><strong>Paranormal:</strong>  <a href="mailto:avery@averyflynn.com?subject=Entry Question">Avery Flynn (avery@averyflynn.com)</a></p>
-    <p><strong>Single Title:</strong>  <a href="mailto:avery@averyflynn.com?subject=Entry Question">Avery Flynn (avery@averyflynn.com)</a></p>
-    <p><strong>Category (Series):</strong>  <a href="mailto:itraci@hotmail.com?subject=Entry Question">Traci Andrighetti (itraci@hotmail.com)</a></p>
-    <p><strong>Historical:</strong>  <a href="mailto:itraci@hotmail.com?subject=Entry Question">Traci Andrighetti (itraci@hotmail.com)</a></p>
-    <p><strong>Inspirational:</strong>  <a href="mailto:itraci@hotmail.com?subject=Entry Question">Traci Andrighetti (itraci@hotmail.com)</a></p>
+    <p><strong>Mainstream:</strong>  <a href="mailto:eliz.sade@gmail.com?subject=Entry Question">Elizabeth Sade (eliz.sade@gmail.com)</a></p>
+    <p><strong>Paranormal:</strong>  <a href="mailto:everin23@gmail.com?subject=Entry Question">Erin Novotny (everin23@gmail.com)</a></p>
+    <p><strong>Single Title:</strong>  <a href="mailto:everin23@gmail.com?subject=Entry Question">Erin Novotny (everin23@gmail.com)</a></p>
+    <p><strong>Category (Series):</strong>  <a href="mailto:eliz.sade@gmail.com?subject=Entry Question">Elizabeth Sade (eliz.sade@gmail.com)</a></p>
+    <p><strong>Historical:</strong>  <a href="mailto:everin23@gmail.com?subject=Entry Question">Erin Novotny (everin23@gmail.com)</a></p>
+    <p><strong>Inspirational:</strong>  <a href="mailto:eliz.sade@gmail.com?subject=Entry Question">Elizabeth Sade (eliz.sade@gmail.com)</a></p>
     <h5><strong>Administrative Coordinators</strong></h5>
 
     <p><strong>Contest Chairman:</strong> <a href="mailto:daphnecontest@gmail.com?subject=Entry Question">Brooke Wills (daphnecontest@gmail.com)</a></p>
