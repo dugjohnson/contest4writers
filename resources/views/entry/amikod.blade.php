f72b6eb00ae9b646500ef2607efee74b9ded4a93
Doug Johnson <doug@asknice.com>
2019-01-16 20:06:08 -0600

    Separate buttons visually

--- resources/views/entry/amikod.blade.php
+++ resources/views/entry/amikod.blade.php
@@ -1,11 +1,10 @@
 @extends('layout-forms')
 
 @section('content')
-        <a class="button radius" href="{!! route('paypal.payment.kodcheck',['entry_id'=>$entry, 'kodmember'=>false])!!}">
-        I am not a KOD Member *
+        <a style="margin: 0 10px;" class="button radius amikod" href="{!! route('paypal.payment.kodcheck',['entry_id'=>$entry, 'kodmember'=>false])!!}">
+        I am not a KOD Member
         </a>
-
-        <a class="button radius"  href="{!! route('paypal.payment.kodcheck',['entry_id'=>$entry, 'kodmember'=>true])!!}">
+        <a style="margin: 0 10px;" class="button radius amikod"  href="{!! route('paypal.payment.kodcheck',['entry_id'=>$entry, 'kodmember'=>true])!!}">
         I am a KOD Member
         </a>
 @stop
