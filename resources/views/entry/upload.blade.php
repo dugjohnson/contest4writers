02210d17c3f1262e85924c2d6a515b8f191c73ef
dugjohnson <doug@asknice.com>
2021-01-15 17:11:06 +0000

    add upload to entry listing for published

--- resources/views/entry/upload.blade.php
+++ resources/views/entry/upload.blade.php
@@ -4,7 +4,11 @@
     {!! Form::open(array('files'=>'true','method'=>'post','url' => '/entries/'.$entry->id.'/upload')) !!}
     {!! Form::hidden('isCoordinator',$isCoordinator) !!}
     @include('errors')
-    @include('entry.fileuploadunpub')
+    @if($entry->published)
+        @include('entry.fileuploadpub')
+    @else
+        @include('entry.fileuploadunpub')
+    @endif
     @include('entry.formclose')
     <hr>
     <strong>Uploading and replacing the file for the following entry</strong>
