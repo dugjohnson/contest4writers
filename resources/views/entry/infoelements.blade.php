9a41bfc7a1a764c58c2b318a8ba578162941473e
dugjohnson <doug@asknice.com>
2021-01-18 20:48:39 +0000

    add elements to email and show page

--- resources/views/entry/infoelements.blade.php
+++ resources/views/entry/infoelements.blade.php
@@ -1,5 +1,5 @@
 <p>Story has elements of</p>
-<p>Sex/sensuality on the page: {!! $entry->erotic==true?"Yes","No"!!}</p>
-<p>LGBTQ+: {!! $entry->glbt==true?"Yes","No" !!}</p>
-<p>Violence (including physical and sexual assault) on the page: {!! $entry->bdsm==true?"Yes","No" !!}</p>
-<p>Child death/near death on the page: {!! $entry->childdeath==true?"Yes","No" !!}</p>
+<p>Sex/sensuality on the page: @if ( $entry->erotic==true )"Yes" @else"No"@endif</p>
+<p>LGBTQ+: @if ( $entry->glbt==true )"Yes" @else"No"@endif</p>
+<p>Violence (including physical and sexual assault) on the page: @if ( $entry->bdsm==true )"Yes" @else"No"@endif</p>
+<p>Child death/near death on the page: @if ( $entry->childdeath==true )"Yes" @else"No"@endif</p>
