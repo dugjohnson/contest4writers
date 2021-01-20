fb1f962d934a26453916060f07e4fbbd8707fc1e
dugjohnson <doug@asknice.com>
2021-01-20 14:12:54 +0000

    fix Coordinators can't edit their profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -6,11 +6,11 @@
 @stop
 
 @section('sidebar')
-    @if ($isCoordinator)
+    @if ($isCoordinator and Auth::id()==$user->id)
         <a href="/coordinators/entries" class="button radius">Back to Coordinator Entries</a>
         <a href="/coordinators/scoresheets" class="button radius">Back to Coordinator Scoresheets</a>
-    @else
+    @endif
+    @if (Auth::id()==$user->id)
         <a class="button radius" href="/users/{{$user->id}}/edit">Edit Profile</a>
     @endif
-
 @stop
