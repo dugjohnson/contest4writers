60b753093242bc0f4138e1ebfba9647f62c17dcf
dugjohnson <doug@asknice.com>
2021-03-26 16:41:01 +0000

    fix grid layout for asssignment

--- dugjohnson-administration/views/admin/scoresheets/list.blade.php
+++ dugjohnson-administration/views/admin/scoresheets/list.blade.php
@@ -5,6 +5,11 @@
     {!! Html::script('js/angular/ui-grid.js') !!}
     {!! Html::style('js/angular/ui-grid.css') !!}
     {!! Html::script('js/assignment.js') !!}
+    <style type="text/css" media="screen">
+        div.ui-grid-header-cell-wrapper{
+            height: auto;
+        }
+    </style>
     <div ng-controller="MainCtrl" class="large-12 columns">
             <strong>Total Scoresheets:</strong> @{{ gridOptions.data.length | number }}
             <div id="scoresheet-grid" ui-grid="gridOptions" ui-grid-edit ui-grid-row-edit ui-grid-cellNav class="scoregrid"></div>
