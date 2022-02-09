f5edfbd437b98fe97482cd0a3c026dadd69a2510
dugjohnson <doug@asknice.com>
2022-02-09 11:52:21 +0000

    update contact page

--- dugjohnson-administration/views/admin/contacts.blade.php
+++ dugjohnson-administration/views/admin/contacts.blade.php
@@ -3,4 +3,18 @@
     <p><strong>Contest Chairperson:</strong>
         <br>
         <a href="mailto:kodcontest@gmail.com?subject=Entry Question">Jennifer Graybeal<br>(kodcontest@gmail.com)</a></p>
+
+    <p><strong>Judge Coordinator:</strong>
+        <br>
+        <a href="mailto:kcr2696439@aol.com?subject=Judge Question">Kathy Crouch<br>(kcr2696439@aol.com)</a></p>
+
+    <p><strong>Category Coordinators:</strong>
+        <br>
+        <strong>Published Division</strong>
+        <br>
+        <a href="mailto:Kimberly.pride@gmail.com?subject=Published Division Question">Kim Pride<br>(kimberly.pride@gmail.com)</a>
+        <br>
+        <strong>Unpublished Division</strong>
+        <br>
+        <a href="mailto:erin.novotny@comcast.net?subject=Unpublished Division Question">Erin Novotny<br>(erin.novotny@comcast.net)</a></p>
 </div>
