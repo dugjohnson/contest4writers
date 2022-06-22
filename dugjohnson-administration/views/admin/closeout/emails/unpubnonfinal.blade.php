ff266903ca0e9e6f61c45d31885d1858a46bd439
doug <doug@asknice.com>
2022-06-22 03:53:00 +0000

    fix closeout emails

--- dugjohnson-administration/views/admin/closeout/emails/unpubnonfinal.blade.php
+++ dugjohnson-administration/views/admin/closeout/emails/unpubnonfinal.blade.php
@@ -1,16 +1,30 @@
 <p>Dear Daphne du Maurier Award for Excellence in Mystery/Suspense Entrant:</p>
 
-<p>The {{ $contest_year }} Daphne Contest first round judging has ended, and the Unpublished Division Finalists have been announced. Unfortunately, you did not final.</p>
+<p>My sincere apologies, everyone. Once the scoresheets were released on Tuesday, June 21, we discovered there were a
+    number of formatting issues. I am so sorry for the confusion and want to thank the folks that brought the errors to
+    my attention. The issues have now been fixed so we are resending all of the scoresheets out again. If you have any
+    questions, please let me know. </p>
+<p>The {{ $contest_year }} Daphne Contest first round judging has ended, and the Unpublished Division Finalists have
+    been announced. Unfortunately, you did not final.</p>
 
-<p>While this is disappointing, we hope you will keep in mind that contests are highly subjective, the Daphne du Maurier Award for Excellence in Mystery/Suspense contest is highly competitive, and what one judge loves, another doesn't.</p>
+<p>While this is disappointing, we hope you will keep in mind that contests are highly subjective, the Daphne du Maurier
+    Award for Excellence in Mystery/Suspense contest is highly competitive, and what one judge loves, another
+    doesn't.</p>
 
-<p>We hope you will take this opportunity to look at the judges’ comments. Occasionally, they will make no sense to you, but other times you may actually have an “Aha moment” to make your manuscript better. In any case, if two or more judges say the same thing, it may be a good idea to consider some editing. It is always, of course, your story.</p>
+<p>We hope you will take this opportunity to look at the judges’ comments. Occasionally, they will make no sense to you,
+    but other times you may actually have an “Aha moment” to make your manuscript better. In any case, if two or more
+    judges say the same thing, it may be a good idea to consider some editing. It is always, of course, your story.</p>
 
-<p>In the rare case your judge did not make comments, please be advised that the Contest Co-Chairs and Coordinators keep track of these judges. We cannot control everything in this contest, but we want to be first and foremost known as the contest that gives feedback.</p>
+<p>In the rare case your judge did not make comments, please be advised that the Contest Co-Chairs and Coordinators keep
+    track of these judges. We cannot control everything in this contest, but we want to be first and foremost known as
+    the contest that gives feedback.</p>
 
 <p>If you wish to send a thank-you note to your judges, you may send them to us and we will forward it.</p>
 
-<p>Thank you for entering the {{ $contest_year }} Daphne du Maurier Award for Excellence in Mystery/Suspense contest. We hope you will join us on Saturday, June 26, 2021, at 4:00pm Pacific Time for the 2021 Daphne du Maurier Award for Excellence Virtual Ceremony where the winners will be announced. More information will be provided soon.</p>
+<p>Thank you for entering the {{ $contest_year }} Daphne du Maurier Award for Excellence in Mystery/Suspense contest.
+    For details about the Daphne du Maurier Award for Excellence Awards Ceremony announcing this year's winners and the
+    opening of next year's contest, please follow the Kiss of Death chapter on Instagram:
+    https://www.instagram.com/kiss_of_death_rwa/ and Twitter: https://twitter.com/RWAKissofDeath.</p>
 
 <p>We wish you the very best in your writing career.</p>
 
