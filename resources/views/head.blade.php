3d4863534f59c404a04f8b60b47746cb213f339d
doug <doug@asknice.com>
2023-02-12 02:31:09 +0000

    fix button problem, css

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,13 +2,14 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="_token" content="{{ csrf_token() }}"/>
-    {!! Html::style('css/foundation.min.css') !!}
     {!! Html::style('css/app.css') !!}
+    {!! Html::style('css/foundation.min.css') !!}
 
+    {!! Html::script('js/app.js') !!}
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     {!! Html::script('js/foundation.min.js') !!}
     {!! Html::script('js/vendor/modernizr.js') !!}
-    {!! Html::script('js/app.js') !!}
+
 
 
 </head>
