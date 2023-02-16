89b3fa56a69057662ce884736ad1240ce8dcb018
doug <doug@asknice.com>
2023-02-16 08:57:08 +0000

    first step to removing foundation

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -2,11 +2,13 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="_token" content="{{ csrf_token() }}"/>
+
+
     <link href="/css/app.css" rel="stylesheet" type="text/css">
     <link href="/css/foundation.min.css" rel="stylesheet" type="text/css">
 
-    <script src="/js/app.js"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="/js/foundation.min.js"></script>
     <script src="/js/vendor/modernizr.js"></script>
+    <script src="/js/app.js"></script>
 </head>
