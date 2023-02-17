0eceb19cbab7f94af16d1e008a71dd2981d5e50c
doug <doug@asknice.com>
2023-02-17 05:08:18 +0000

    fix edit elements

--- resources/views/entry/infoelements.blade.php
+++ resources/views/entry/infoelements.blade.php
@@ -1,5 +1,5 @@
 <p>Story has elements of</p>
-<p>Sex/sensuality on the page: @if ( $entry->erotic==true )"Yes" @else"No"@endif</p>
-<p>LGBTQ+: @if ( $entry->glbt==true )"Yes" @else"No"@endif</p>
-<p>Violence (including physical and sexual assault) on the page: @if ( $entry->bdsm==true )"Yes" @else"No"@endif</p>
-<p>Child death/near death on the page: @if ( $entry->childdeath==true )"Yes" @else"No"@endif</p>
+<p>Sex/sensuality on the page: @if ( $entry->erotic==1 )"Yes" @else"No"@endif</p>
+<p>LGBTQ+: @if ( $entry->glbt==1 )"Yes" @else"No"@endif</p>
+<p>Violence (including physical and sexual assault) on the page: @if ( $entry->bdsm==1 )"Yes" @else"No"@endif</p>
+<p>Child death/near death on the page: @if ( $entry->childdeath==1 )"Yes" @else"No"@endif</p>
