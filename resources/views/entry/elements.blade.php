422bc3b057f39e2798a43bcf3b686d1c50f4fa9c
doug <doug@asknice.com>
2023-02-17 05:06:42 +0000

    fix edit elements

--- resources/views/entry/elements.blade.php
+++ resources/views/entry/elements.blade.php
@@ -3,22 +3,22 @@
     <!-- Erotic Form Input -->
     <div class="form-group">
         {!! Form::label('erotic', 'Sex/sensuality on the page:') !!}
-        {!! Form::radio('erotic',true, $entry->erotic==true, ['class' => 'form-control']) !!}
-        Yes {!! Form::radio('erotic',false, $entry->erotic==false, ['class' => 'form-control']) !!} No
+        {!! Form::radio('erotic',true, $entry->erotic==1, ['class' => 'form-control']) !!}
+        Yes {!! Form::radio('erotic',false, $entry->erotic==0, ['class' => 'form-control']) !!} No
     </div>
     <div class="form-group">
         {!! Form::label('glbt', 'LGBTQ+:') !!}
-        {!! Form::radio('glbt',true, $entry->glbt==true, ['class' => 'form-control']) !!}
-        Yes {!! Form::radio('glbt',false, $entry->glbt==false, ['class' => 'form-control']) !!} No
+        {!! Form::radio('glbt',true, $entry->glbt==1, ['class' => 'form-control']) !!}
+        Yes {!! Form::radio('glbt',false, $entry->glbt==0, ['class' => 'form-control']) !!} No
     </div>
     <div class="form-group">
         {!! Form::label('bdsm', 'Violence (including physical and sexual assault) on the page:') !!}
-        {!! Form::radio('bdsm',true, $entry->bdsm==true, ['class' => 'form-control']) !!}
-        Yes {!! Form::radio('bdsm',false, $entry->bdsm==false, ['class' => 'form-control']) !!} No
+        {!! Form::radio('bdsm',true, $entry->bdsm==1, ['class' => 'form-control']) !!}
+        Yes {!! Form::radio('bdsm',false, $entry->bdsm==0, ['class' => 'form-control']) !!} No
     </div>
     <div class="form-group">
         {!! Form::label('childdeath', 'Child death/near death on the page:') !!}
-        {!! Form::radio('childdeath',true, $entry->childdeath==true, ['class' => 'form-control']) !!}
-        Yes {!! Form::radio('childdeath',false, $entry->childdeath==false, ['class' => 'form-control']) !!} No
+        {!! Form::radio('childdeath',true, $entry->childdeath==1, ['class' => 'form-control']) !!}
+        Yes {!! Form::radio('childdeath',false, $entry->childdeath==0, ['class' => 'form-control']) !!} No
     </div>
 </div>
