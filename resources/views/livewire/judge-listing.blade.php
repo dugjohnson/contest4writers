c75b2bd865a6f88fef985c9814b13e7f598ed40e
Doug Johnson <doug@asknice.com>
2023-02-22 15:42:20 +0530

    Add judging preferences

--- resources/views/livewire/judge-listing.blade.php
+++ resources/views/livewire/judge-listing.blade.php
@@ -15,6 +15,12 @@
                             </x-search-input>
                         </div>
                     </div>
+                    <div class="divide-y my-4 divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+                        <div class="px-4 py-5 sm:p-6">
+                            <p>Judging preferences</p>
+                            <p>4 = Top choice, 3 = Love to judge, 2 = Would judge if asked, 1 = Would judge in an emergency, 0 = Will not judge</p>
+                        </div>
+                    </div>
                     <table class="divide-y divide-gray-300">
                         <thead class="bg-gray-50">
                         <tr>
@@ -38,6 +44,30 @@
                                 class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
                                 Action
                             </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Mainstream
+                            </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Long
+                            </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Short
+                            </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Novella
+                            </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Historical
+                            </th>
+                            <th scope="col"
+                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 whitespace-nowrap">
+                                Paranormal
+                            </th>
                         </tr>
                         </thead>
                         <tbody>
@@ -54,6 +84,13 @@
                                     <a href="/coordinators/judges/{{ $judge->id }}">Show</a>
                                     / <a href="/coordinators/judges/{{ $judge->id }}/edit">Edit</a>
                                     / <a href="/scoresheets/judge/{{ $judge->id }}/comparison">Compare Scores</a></td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->mainstream }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->longTitle }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->shortTitle }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->novella }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->historical }}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ $judge->paranormal }}</td>
+
                             </tr>
                         @endforeach
                         </tbody>
