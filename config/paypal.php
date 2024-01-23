ddcacf4f59b0d9c42d79b74c43e9c8ce42b1059b
Doug Johnson <doug@asknice.com>
2024-01-23 16:39:07 -0600

    remove secret

--- config/paypal.php
+++ config/paypal.php
@@ -12,8 +12,8 @@ return [
         'app_id'            => 'KODContest-Sandbox',
     ],
     'live' => [
-        'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', 'Ad9JRUTfFYjeb1Brds7etUJnVFjAc2L7jjcLxJF9TryrrZKp6C_a2u5dYbr0EhIxKPkyQvb90uEijdXM'),
-        'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET', 'EBLlfcI9xW0XwbjQv9_9HgIiIfgxql8PQZAI4anaoZZ8pYY2Nux3CT4nGuwtma7sqRDDhb8fHz-kuX0f'),
+        'client_id'         => env('PAYPAL_LIVE_CLIENT_ID', ''),
+        'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET', ''),
         'app_id'            => env('PAYPAL_LIVE_APP_ID', 'KODContest'),
     ],
 
