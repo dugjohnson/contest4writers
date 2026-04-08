18e9adb2255a427f1eb9400c1c74a7f80a11c075
Doug Johnson <doug@asknice.com>
2026-04-08 13:06:08 -0500

    make changes to scoresheet text form

--- resources/views/scoresheets/show/openunpub.blade.php
+++ resources/views/scoresheets/show/openunpub.blade.php
@@ -13,12 +13,12 @@
         <li>2 = fair</li>
         <li>1 = poor</li>
     </ul>
-    @if(! ($email ?? false))
-        <p class="text-red-500">Are there any concerns regarding category or content for this entry?
-            Any concerns to share with the contest committee? Please let us know if this book is entered in the wrong
-            category, contains language or situations that are culturally or racially offensive, or is otherwise
-            problematic.</p>
-        <p><span class="text-red-500">Comments (leave blank if no issues)</span>: {{ $scoresheet->sheet->comments->comment01 }} </p>
-    @endif
+{{--    @if(! ($email ?? false))--}}
+{{--        <p class="text-red-500">Are there any concerns regarding category or content for this entry?--}}
+{{--            Any concerns to share with the contest committee? Please let us know if this book is entered in the wrong--}}
+{{--            category, contains language or situations that are culturally or racially offensive, or is otherwise--}}
+{{--            problematic.</p>--}}
+{{--        <p><span class="text-red-500">Comments (leave blank if no issues)</span>: {{ $scoresheet->sheet->comments->comment01 }} </p>--}}
+{{--    @endif--}}
 
 </fieldset>
