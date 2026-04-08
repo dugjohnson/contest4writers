12daed768d41d75310883e23d7fdb582f116ba58
Doug Johnson <doug@asknice.com>
2026-04-08 17:15:24 -0500

    make comments fields be required

--- resources/views/scoresheets/edit/formclose.blade.php
+++ resources/views/scoresheets/edit/formclose.blade.php
@@ -1,7 +1,10 @@
 <fieldset>
     <legend>Save or Save & Complete</legend>
     <!--see scoresheet.js for handling of submits-->
+    <p>All questions must have a score and all comments must be completed before scoresheet can be marked complete.
+    The Scoresheet is Complete button will be activated when you are done with all questions and comments.</p>
     <input type="hidden" name="process_method" id="process_method" value="">
     <input type="submit" id="submitButton" name="submitButton" class="button radius" value="Save Edits">
     <input type="submit" id="completeButton" name="completeButton" class="button radius right" value="Scoresheet is Complete">
+    <p>If you think you have finished, but the button is not activated, hit the tab key. If it is still not activated, please check that all questions have scores and all comments are completed.</p>
 </fieldset>
